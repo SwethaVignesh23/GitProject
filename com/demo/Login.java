43db3604a460912e33802591e4adfbd0fcdb7e99
SwethaVignesh23 <swethaforjob98@gmail.com>
2024-05-08 18:58:36 +0530

    bew

--- com/demo/Login.java
+++ com/demo/Login.java
@@ -4,5 +4,10 @@ public class Login {
 	public static void main(String[] args) {
 		System.out.println("Hai Swetha");
 	}
+	
+	private void username() {
+		System.out.println("create user name");
+
+	}
 
 }
